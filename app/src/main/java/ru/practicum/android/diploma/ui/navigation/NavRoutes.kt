cfec1f0c5ca1c238c5be9601a8b3412fba4d8637
Andrey Pchelintsev <doctor.madbee@gmail.com>
2026-02-22 20:13:23 +0700

    минорные исправления для detekt

--- app/src/main/java/ru/practicum/android/diploma/ui/navigation/NavRoutes.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.ui.navigation
-
-sealed class NavRoute(val route: String) {
-    object Search: NavRoute("search")
-    object Favorites: NavRoute("favorites")
-    object Team: NavRoute("team")
-    object VacancyDetails: NavRoute("vacancy/{id}")
-    object Filter: NavRoute("filter")
-}
