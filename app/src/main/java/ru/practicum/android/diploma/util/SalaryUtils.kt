3c2a083801d0a96a136ed396061a6d1025fe8cbe
LampJuice <dieez347@yandex.ru>
2026-02-22 22:51:47 +0300

    detekt исправления

--- app/src/main/java/ru/practicum/android/diploma/util/SalaryUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/SalaryUtils.kt
@@ -1,4 +1,5 @@
 package ru.practicum.android.diploma.util
+
 /**
  * Форматирует число в строку с пробелами между разраядами
  */
@@ -7,6 +8,11 @@ fun formatNumber(value: Int?): String {
     return "%,d".format(value).replace(",", " ")
 }
 
+data class SalaryStrings(
+    val from: String,
+    val to: String,
+    val noSalary: String
+)
 /**
  * @param min - минимальная зарплата
  * @param max - максимальная зарплата
@@ -16,18 +22,16 @@ fun formatSalaryRange(
     min: Int?,
     max: Int?,
     currency: Currency?,
-    fromText: String,
-    toText: String,
-    noSalaryText: String
-    ): String {
+    strings: SalaryStrings
+): String {
     val minStr = min?.let { formatNumber(it) } ?: ""
     val maxStr = max?.let { formatNumber(it) } ?: ""
     val symbol = currency?.symbol ?: ""
 
     return when {
-        min != null && max != null -> "$fromText $minStr $toText $maxStr $symbol"
-        min != null -> "$fromText $minStr $symbol"
-        max != null -> "$toText $maxStr $symbol"
-        else -> noSalaryText
+        min != null && max != null -> "${strings.from} $minStr ${strings.to} $maxStr $symbol"
+        min != null -> "${strings.from} $minStr $symbol"
+        max != null -> "${strings.to} $maxStr $symbol"
+        else -> strings.noSalary
     }
 }
