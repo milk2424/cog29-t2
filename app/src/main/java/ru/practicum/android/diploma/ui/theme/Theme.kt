867696fa9b7bffd887fe17a3b9c471c0b64159c4
ProklB <proklb@ya.ru>
2026-02-23 01:40:05 +0500

    исправления после детекта v02

--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
@@ -1,9 +1,9 @@
 package ru.practicum.android.diploma.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 private val LightColorScheme = lightColorScheme(
