93ad9a5d545f087ba4cea0020d30b3e604830749
ProklB <proklb@ya.ru>
2026-02-26 20:04:32 +0500

    SearchViewModel Взял за основу наработку LampJuice, учел замечания

--- app/src/main/java/ru/practicum/android/diploma/di/PresentationModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/PresentationModule.kt
@@ -3,9 +3,14 @@ package ru.practicum.android.diploma.di
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.team.TeamViewModel
+import ru.practicum.android.diploma.ui.screens.search.SearchViewModel
 
 val presentationModule = module {
     viewModel {
         TeamViewModel(get(), get())
+    }
+
+    viewModel {
+        SearchViewModel(searchInteractor = get())
     }
 }
