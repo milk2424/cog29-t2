4e57eea0374cb828e11f3c3f945a632d4c1026b4
Dmitry <milko.dimon@mail.ru>
2026-02-26 14:07:37 +0300

    add data/domain logic + viewModel

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/team/model/Developer.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.domain.team.model
+
+import androidx.annotation.StringRes
+
+data class Developer(
+    @StringRes val nameRes: Int,
+    @StringRes val githubLinkRes: Int
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/team/use_case/LoadDevelopersTeamUseCase.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.domain.team.use_case
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.team.model.Developer
+import ru.practicum.android.diploma.domain.team.repository.TeamRepository
+
+class LoadDevelopersTeamUseCase(private val repository: TeamRepository) {
+    operator fun invoke(): Flow<List<Developer>> = repository.getTeamDevelopers()
+}
+
