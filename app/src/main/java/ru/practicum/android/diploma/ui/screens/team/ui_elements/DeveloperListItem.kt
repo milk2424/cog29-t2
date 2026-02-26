fa94f1f1bd0ba61194015271cfc8267fdc977ec6
Dmitry <milko.dimon@mail.ru>
2026-02-26 15:03:17 +0300

    add team screen UI

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/team/ui_elements/DeveloperListItem.kt
@@ -0,0 +1,41 @@
+package ru.practicum.android.diploma.ui.screens.team.ui_elements
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.domain.team.model.Developer
+import ru.practicum.android.diploma.ui.theme.Dimens.iconMedium
+import ru.practicum.android.diploma.ui.theme.Dimens.paddingSmall
+
+@Composable
+fun DeveloperListItem(developer: Developer) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = paddingSmall),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = stringResource(developer.nameRes),
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.titleMedium
+        )
+        Icon(
+            modifier = Modifier.size(iconMedium),
+            painter = painterResource(R.drawable.ic_github),
+            contentDescription = stringResource(R.string.github_link),
+            tint = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Dimens.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Dimens.kt
@@ -4,4 +4,7 @@ import androidx.compose.ui.unit.dp
 
 object Dimens {
     val bottomNavBarDividerThickness = 1.dp
+    val paddingMedium = 16.dp
+    val paddingSmall = 8.dp
+    val iconMedium = 24.dp
 }
