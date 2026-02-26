fb854f527910b4a5119418418270ccfe6401a5c4
Dmitry Milko <milko.dimon@mail.ru>
2026-02-27 00:41:53 +0300

    network global refactor

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/search/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.ui.screens.search
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
