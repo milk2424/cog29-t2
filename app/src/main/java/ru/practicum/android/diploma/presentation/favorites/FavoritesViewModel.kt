5d4aa78fdb357416cf80ba67d26d1f85eca06ca4
Dmitry <milko.dimon@mail.ru>
2026-02-27 14:05:14 +0300

    dtkt fix v3

--- app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoritesViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoritesViewModel.kt
@@ -2,5 +2,4 @@ package ru.practicum.android.diploma.presentation.favorites
 
 import androidx.lifecycle.ViewModel
 
-class FavoritesViewModel : ViewModel() {
-}
+class FavoritesViewModel : ViewModel()
