33b75523523781b19976baf167442e322732cf03
grey-bee <doctor.madbee@gmail.com>
2026-02-28 14:28:18 +0700

    toggle Для favorite

--- app/src/main/java/ru/practicum/android/diploma/domain/api/FavoritesInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/FavoritesInteractor.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.Vacancy
 
 interface FavoritesInteractor {
+    suspend fun toggleFavorite(vacancy: Vacancy)
     fun getAll(): Flow<List<Vacancy>>
     suspend fun add(vacancy: Vacancy)
     suspend fun remove(vacancyId: String)
--- app/src/main/java/ru/practicum/android/diploma/domain/impl/FavoritesInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/FavoritesInteractorImpl.kt
@@ -5,6 +5,13 @@ import ru.practicum.android.diploma.domain.api.FavoritesRepository
 import ru.practicum.android.diploma.domain.models.Vacancy
 
 class FavoritesInteractorImpl(private val repository: FavoritesRepository) : FavoritesInteractor {
+    override suspend fun toggleFavorite(vacancy: Vacancy) {
+        if (repository.isFavorite(vacancy.id)) {
+            repository.delete(vacancy.id)
+        } else {
+            repository.add(vacancy)
+        }
+    }
     override fun getAll() = repository.getAll()
     override suspend fun add(vacancy: Vacancy) = repository.add(vacancy)
     override suspend fun remove(vacancyId: String) = repository.delete(vacancyId)
