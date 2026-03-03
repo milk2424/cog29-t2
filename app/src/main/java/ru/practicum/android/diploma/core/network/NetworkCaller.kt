d9b3bfd5212436d275a685361caa403fee1c1561
atom-327 <machoman1488@yandex.ru>
2026-03-04 02:12:35 +0400

    Detekt_fix

--- app/src/main/java/ru/practicum/android/diploma/core/network/NetworkCaller.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/NetworkCaller.kt
@@ -1,9 +1,15 @@
 package ru.practicum.android.diploma.core.network
 
+import android.util.Log
 import retrofit2.Response
 import ru.practicum.android.diploma.domain.utils.ApiResult
+import java.io.IOException
 
 class NetworkCaller(private val networkChecker: NetworkChecker) {
+    companion object {
+        private const val HTTP_NOT_FOUND = 404
+    }
+
     suspend fun <T, R> safeApiCall(
         apiCall: suspend () -> Response<T>,
         transform: (T) -> R
@@ -22,7 +28,7 @@ class NetworkCaller(private val networkChecker: NetworkChecker) {
                     }
                 }
 
-                response.code() == 404 -> {
+                response.code() == HTTP_NOT_FOUND -> {
                     ApiResult.NotFoundError
                 }
 
@@ -30,7 +36,11 @@ class NetworkCaller(private val networkChecker: NetworkChecker) {
                     ApiResult.ServerError(code = response.code())
                 }
             }
+        } catch (e: IOException) {
+            Log.e("NetworkCaller", "Network error", e)
+            ApiResult.NetworkError
         } catch (e: Exception) {
+            Log.e("NetworkCaller", "Unknown error", e)
             ApiResult.UnknownError
         }
     }
