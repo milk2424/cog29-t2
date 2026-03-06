2f787b1b94c19d9c518ea389bd8ea4eb4f7315aa
grey-bee <doctor.madbee@gmail.com>
2026-03-06 13:56:20 +0700

    разобрал NetworkCaller на интерфейс и имлиментацию

--- app/src/main/java/ru/practicum/android/diploma/core/network/NetworkCaller.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/NetworkCaller.kt
@@ -1,44 +1,11 @@
 package ru.practicum.android.diploma.core.network
 
-import android.util.Log
-import com.google.gson.JsonParseException
 import retrofit2.Response
 import ru.practicum.android.diploma.domain.utils.ApiResult
 
-class NetworkCaller(private val networkChecker: NetworkChecker) {
-    companion object {
-        private const val HTTP_NOT_FOUND = 404
-    }
-
+interface NetworkCaller {
     suspend fun <T, R> safeApiCall(
         apiCall: suspend () -> Response<T>,
         transform: (T) -> R
-    ): ApiResult<R> {
-        if (!networkChecker.isNetworkAvailable()) return ApiResult.NetworkError
-        return try {
-            val response = apiCall()
-            when {
-                response.isSuccessful -> {
-                    val resultData = response.body()
-                    if (resultData == null) {
-                        ApiResult.ServerError(code = response.code())
-                    } else {
-                        val mappedData = transform(resultData)
-                        ApiResult.Success(mappedData)
-                    }
-                }
-
-                response.code() == HTTP_NOT_FOUND -> {
-                    ApiResult.NotFoundError
-                }
-
-                else -> {
-                    ApiResult.ServerError(code = response.code())
-                }
-            }
-        } catch (e: JsonParseException) {
-            Log.e("NetworkCaller", "JSON parsing error", e)
-            ApiResult.UnknownError
-        }
-    }
+    ): ApiResult<R>
 }
