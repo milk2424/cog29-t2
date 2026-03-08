4732e3b17ce70224e12bd6705e5bb2a906e32634
Andrey Pchelintsev <doctor.madbee@gmail.com>
2026-03-08 20:58:23 +0700

    исправил импорты в PresentationModule.kt

--- app/src/main/java/ru/practicum/android/diploma/di/PresentationModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/PresentationModule.kt
@@ -5,8 +5,8 @@ import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.favorites.FavoritesViewModel
 import ru.practicum.android.diploma.presentation.filter.FilterViewModel
 import ru.practicum.android.diploma.presentation.filter.country.CountrySelectionViewModel
-import ru.practicum.android.diploma.presentation.filter.workplace.WorkplaceSelectionViewModel
 import ru.practicum.android.diploma.presentation.filter.industry.IndustrySelectionViewModel
+import ru.practicum.android.diploma.presentation.filter.workplace.WorkplaceSelectionViewModel
 import ru.practicum.android.diploma.presentation.search.SearchViewModel
 import ru.practicum.android.diploma.presentation.team.TeamViewModel
 import ru.practicum.android.diploma.presentation.vacancy.VacancyViewModel
